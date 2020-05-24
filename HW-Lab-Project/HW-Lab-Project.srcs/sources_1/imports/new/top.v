7a8e418fd38b94c62f7044b00ffb2931cfc220d7
David Tumcharoen <d19450t@gmail.com>
2020-05-25 00:26:05 +0700

    Create Image Modules

--- HW-Lab-Project/HW-Lab-Project.srcs/sources_1/imports/new/top.v
+++ HW-Lab-Project/HW-Lab-Project.srcs/sources_1/imports/new/top.v
@@ -1,28 +1,78 @@
 `timescale 1ns / 1ps
 
 module top(
-    input clk,
-    output Hsync,
-    output Vsync,
-    output [3:0] Red,
-    output [3:0] Green,
-    output [3:0] Blue
+    input wire CLK,             // board clock: 100 MHz on Arty/Basys3/Nexys
+    input wire RST_BTN,         // reset button
+    output wire VGA_HS_O,       // horizontal sync output
+    output wire VGA_VS_O,       // vertical sync output
+    output reg [3:0] VGA_R,     // 4-bit VGA red output
+    output reg [3:0] VGA_G,     // 4-bit VGA green output
+    output reg [3:0] VGA_B      // 4-bit VGA blue output
     );
-    
-wire clk_25M;
-wire enable_V_Counter;
-wire [15:0] H_Count_Value;
-wire [15:0] V_Count_Value;
 
-clock_divider VGA_Clock_gen (clk, clk_25M);
-horizontal_counter VGA_Horiz (clk_25M, enable_V_Counter, H_Count_Value);
-vertical_counter VGA_Verti (clk_25M, enable_V_Counter, V_Count_Value);
+    wire rst = RST_BTN;  // reset is active high on Basys3 (BTNC)
 
-assign Hsync = (H_Count_Value < 96) ? 1'b1:1'b0;
-assign Vsync = (V_Count_Value < 2) ? 1'b1:1'b0;
+    reg [15:0] cnt;
+    reg pix_stb;
+    always @(posedge CLK)
+        {pix_stb, cnt} <= cnt + 16'h6666; 
 
-assign Red = (H_Count_Value < 784 && H_Count_Value > 143 && V_Count_Value < 300 && V_Count_Value > 34) ? 4'hF:4'h0;
-assign Green = (H_Count_Value < 784 && H_Count_Value > 143 && V_Count_Value < 400 && V_Count_Value > 300) ? 4'hF:4'h0;
-assign Blue = (H_Count_Value < 784 && H_Count_Value > 143 && V_Count_Value < 515 && V_Count_Value > 400) ? 4'hF:4'h0;
+    wire [10:0] x;  // current pixel x position: 10-bit value: 0-2047
+    wire [9:0] y;  // current pixel y position:  9-bit value: 0-1023
+    wire active;   // high during active pixel drawing
 
+    vga800x600 display (
+        .i_clk(CLK), 
+        .i_pix_stb(pix_stb),
+        .i_rst(rst),
+        .o_hs(VGA_HS_O), 
+        .o_vs(VGA_VS_O), 
+        .o_x(x), 
+        .o_y(y),
+        .o_active(active)
+    );
+
+    // VRAM frame buffers (read-write)
+    localparam SCREEN_WIDTH = 800;
+    localparam SCREEN_HEIGHT = 600;
+    localparam VRAM_DEPTH = SCREEN_WIDTH * SCREEN_HEIGHT; 
+    localparam VRAM_A_WIDTH = 19;  // 2^18 > 640 x 360
+    localparam VRAM_D_WIDTH = 3;   // colour bits per pixel
+
+    reg [VRAM_A_WIDTH-1:0] address;
+    wire [VRAM_D_WIDTH-1:0] dataout;
+
+    sram #(
+        .ADDR_WIDTH(VRAM_A_WIDTH), 
+        .DATA_WIDTH(VRAM_D_WIDTH), 
+        .DEPTH(VRAM_DEPTH), 
+        .MEMFILE("basys.mem"))  // bitmap to load
+        vram (
+        .i_addr(address), 
+        .i_clk(CLK), 
+        .i_write(0),  // we're always reading
+        .i_data(0), 
+        .o_data(dataout)
+    );
+
+    reg [11:0] palette [0:7];  // 8 x 12-bit colour palette entries
+    reg [11:0] colour;
+    initial begin
+        $display("Loading palette.");
+        $readmemh("basys_palette.mem", palette);  // bitmap palette to load
+    end
+
+    always @ (posedge CLK)
+    begin
+        address <= y * SCREEN_WIDTH + x;
+
+        if (active)
+            colour <= palette[dataout];
+        else    
+            colour <= 0;
+
+        VGA_R <= colour[11:8];
+        VGA_G <= colour[7:4];
+        VGA_B <= colour[3:0];
+    end
 endmodule
